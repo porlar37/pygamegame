05e7912a183c86b87591bc9c0318c68e42ad5a5d
porlar37 <jonatanhakansson@protonmail.com>
2016-04-22 17:29:22 +0200

    Added files via upload

---
+++ pygamegame-updater.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd /
+sudo mkdir pygamegame
+wget https://codeload.github.com/porlar37/pygamegame/zip/master
+sudo rm /pygame
+sudo apt-get install unzip
+unzip pygamegame-master -p /pygamegame
+
---
+++ pygamegame.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python /pygamegame/scrips/main.py
