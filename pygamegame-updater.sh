f64ed808031e8634fffea16f29972ffd7f9a2c30
porlar37 <jonatanhakansson@protonmail.com>
2016-04-22 20:48:52 +0200

    Update pygamegame-updater.sh

--- pygamegame-updater.sh
+++ pygamegame-updater.sh
@@ -3,6 +3,7 @@ cd /
 sudo mkdir pygamegame
 wget https://codeload.github.com/porlar37/pygamegame/zip/master
 sudo rm /pygame
+sudo mkdir pygamegame
 sudo apt-get install unzip
 unzip pygamegame-master -p /pygamegame
 
