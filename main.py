4bd05f933dfa888e19de92de75fe498e8d59c002
porlar37 <jonatanhakansson@protonmail.com>
2016-04-25 15:29:59 +0200

    Added files via upload

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pygame, time, random, map.py, items.py, sys, thread
+import pygame, time, random, sys, thread
 from pygame.locals import *
 
 # set up pygame
@@ -17,30 +17,67 @@ exiting = False
 runing = True
 
 #set up esantal veriabels for the player
-player-x = 0
-player-y = 0
-waking-forword = False
-waking-backword = False
-waking-left = False
-waking-right = False
+playerx = 0
+playery = 0
+wakingforword = False
+wakingbackword = False
+wakingleft = False
+wakingright = False
 atacking = False
 
 #defines funcktione for rendering
 def render():
-	
+	#(not done yet)Redys the displaybufer for update	
+
+	#updates the display
+	pygame.display.update()
 #run the game loop
 while runing == True:
 	#check for pygame events
 	for event in pygame.event.get():
+
 		#check for the QUIT event
 		if event.type == QUIT:
+
 			if event.type == QUIT:
 				runing = False
+
 		#check for the KEYDOWN event
 		if event.type == KEYDOWN:
+			#do "" if "" key is prest
+			if event.key == ord("w"):
+				wakingforword = True
+			if event.key == ord("s"):
+				wakingbackword = True
+			if event.key == ord("a"): 
+				wakingleft = True
+			if event.key == ord("d"):
+				wakingright = True
 		#check for the KEYUP event
 		if event.type == KEYUP:
-	thread.start_new_thread(render())
+			#do "" if "" key is stopt being prest
+			if event.key == ord("w"):
+				wakingforword = False
+			if event.key == ord("s"):
+				wakingbackword = False
+			if event.key == ord("a"):
+				wakingleft = False
+			if event.key == ord("d"):
+				wakingright = False
+
+	#change position variabels
+	if wakingforword == True:
+		playerx = playerx + 1
+	if wakingbackword == True:
+		playerx = playerx - 1
+	if wakingleft == True:
+		playery = playery - 1
+	if wakingright == True:
+		platery = playery + 1
+
+	#renders the imge
+	thread.start_new_thread(render(), )
+
 #main loop ended quiting
 pygame.quit()
 sys.quit()
